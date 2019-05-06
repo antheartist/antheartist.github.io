4563cd43fb5b1c63ccb26450d6bd14bb64bdb2e8
antheartist <47002280+antheartist@users.noreply.github.com>
2019-05-06 08:30:48 -0400

    Add files via upload

---
+++ WebCollectionProject/script.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+         
+                  $("#red").hover(function() {
+                   
+                  $("r2").show();}, //hover over
+ 
+                  function() {
+                   
+                  $("r2").hide();   //not hovered
+     });
+            });
+ 
+
+
+/*$(document).ready(function() {
+  $(".red").mouseenter(function() {
+    $(".r2").toggleClass("opacity");
+  });
+
+/*var theParent = document.querySelector(#red);
+the.addEventListener("click, hide, false);
+
+  function hide(element){
+  	if (element.target !== element.currentTarget){
+  		var clickedItem = element.target.id;
+  	}
+
+  }
+
+/*function showBoxes(){
+
+	var element = document.getElementByID("boxes").style.visibility = 'visible';}
+
+	function hideBoxes(){
+
+	var element = document.getElementByID("boxes").style.visibility = 'hidden';}
+	/*if (element.style.visibility == 'hidden') {
+		element.style.visibility == 'visible';
+	}else{ 
+		element.style.visibility = 'hidden';
+
+	}
+}
+
+
+		//onmouseover="show(.boxes)" onmouseout="hide(.boxes)"//$(document).ready(function() {
+//$(".red").mouseOver("boxes");
+//$(".red").mouseOut("boxes");
+ /* $(".boxes").hide(function() {
+    
+  $(".red").onmouseover(showboxes() { */
+  
+  
+  /* .onmouseout(function(){
+  $(".elevator").click(function() {
+    $(".elevator").toggleClass("dark");*/
+ // });
+//
+/*$(“.boxes”).addClass(“function”);
+
+function showboxes()*/
